a319e54d1b6fd40a699bd0622e8525bcfd1016af
Habib710 <mdhabib71024@gmail.com>
2026-01-22 10:59:35 +0600

    booking test managed

--- tests/AlphaHomePage.spec.js
+++ tests/AlphaHomePage.spec.js
@@ -8,57 +8,57 @@ test('Alpha Page and Title', async ({ page }) => {
   await expect(page.getByRole('heading', { name: 'Search Your Room' })).toBeVisible()
 });
 
-// test('Find Room Booking Page', async ({ page }) => {
-//   await page.goto('https://hotel.salonpro.info/');
+test('Find Room Booking Page', async ({ page }) => {
+  await page.goto('https://hotel.salonpro.info/');
 
-//   await page.getByRole('link', { name: 'Find A Room' }).click();
+  await page.getByRole('link', { name: 'Find A Room' }).click();
 
-//   await expect(
-//     page.getByRole('heading', { name: 'Search Your Room' })
-//   ).toBeVisible();
-
-
-//   await page.locator("//input[@placeholder='Check In']").click();
+  await expect(
+    page.getByRole('heading', { name: 'Search Your Room' })
+  ).toBeVisible();
 
 
-//   while (!(await page.locator("text=January 2026").isVisible())) {
-//     await page.locator("button:has-text('›')").click();
-//   }
-//   await page.locator("button:has-text('22')").click();
-
-//   await page.locator("//input[@placeholder='Check In']").click();
+  await page.locator("//input[@placeholder='Check In']").click();
 
 
-//   while (!(await page.locator("text=January 2026").isVisible())) {
-//     await page.locator("button:has-text('›')").click();
-//   }
-//   await page.locator("button:has-text('26')").click();
+  while (!(await page.locator("text=January 2026").isVisible())) {
+    await page.locator("button:has-text('›')").click();
+  }
+  await page.locator("button:has-text('22')").click();
+
+  await page.locator("//input[@placeholder='Check In']").click();
 
 
-
-//   await page.locator("//input[@placeholder='Adults']").fill("2");
-//   await page.locator("//input[@placeholder='Adults']").press("Tab");
-
-//   await page.locator("//input[@placeholder='Children']").fill("1");
-//   await page.locator("//input[@placeholder='Children']").press("Tab");
-
-//   await page.getByRole('button', { name: 'Search' }).click();
-
-
-//   await page.waitForLoadState("networkidle");
-
-
-//   const allCard = page.locator("#level_sum_all");
-//   await expect(allCard).toBeVisible();
-
-
-//   const rawText = (await allCard.innerText()).trim();
-//   console.log("All Card Raw Text:", rawText);
+  while (!(await page.locator("text=January 2026").isVisible())) {
+    await page.locator("button:has-text('›')").click();
+  }
+  await page.locator("button:has-text('26')").click();
 
 
 
+  await page.locator("//input[@placeholder='Adults']").fill("2");
+  await page.locator("//input[@placeholder='Adults']").press("Tab");
 
-// });
+  await page.locator("//input[@placeholder='Children']").fill("1");
+  await page.locator("//input[@placeholder='Children']").press("Tab");
+
+  await page.getByRole('button', { name: 'Search' }).click();
+
+
+  await page.waitForLoadState("networkidle");
+
+
+  const allCard = page.locator("#level_sum_all");
+  await expect(allCard).toBeVisible();
+
+
+  const rawText = (await allCard.innerText()).trim();
+  console.log("All Card Raw Text:", rawText);
+
+
+
+
+});
 
 test('expolor section test', async ({ page }) => {
 
