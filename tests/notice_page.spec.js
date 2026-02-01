2598df78478b436c14b6d46246e179b2986c2719
Habib710 <mdhabib71024@gmail.com>
2026-02-01 12:19:44 +0600

    new page added to test

---
+++ tests/notice_page.spec.js
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+
+test('Alpha Page and Title', async ({ page }) => {
+    await page.goto('https://hotel.salonpro.info/');
+
+
+    await expect(page.getByRole('heading', { name: 'Search Your Room' })).toBeVisible()
+});
