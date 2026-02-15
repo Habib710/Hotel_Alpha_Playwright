becc0e8606f24305fac9cf1b5e323c633354992f
Habib710 <mdhabib71024@gmail.com>
2026-02-15 16:51:35 +0600

    error resloved

--- tests/section.spec.js
+++ tests/section.spec.js
@@ -1,27 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-export class AddtoCardObject {
-
-    constructor(page) {
-        this.page = page;
-        this.locator = new HomePageObjects(page);
-    }
-
-    test('Alpha Page and Title', async ({ page }) => {
-        await page.goto('https://hotel.salonpro.info/');
 
 
-        await expect(page.getByRole('heading', { name: 'Search Your Room' })).toBeVisible()
-    });
-
-test('Alpha ', async ({ page }) => {
+test('Alpha Page and Title', async ({ page }) => {
     await page.goto('https://hotel.salonpro.info/');
 
 
-    await expect(page.getByText('© ALPHA All Rights Reserved. Developed By HRSOFTBD')).toBeVisible()
+    await expect(page.getByRole('heading', { name: 'Search Your Room' })).toBeVisible()
+});
+
+test('section part test', async ({ page }) => {
+    await page.goto('https://hotel.salonpro.info/');
 
 });
 
 
-}
+
+
 
